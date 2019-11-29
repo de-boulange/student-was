42aa9846c337a01676fb7d3d0507b1d57350c893
de-boulange <bartosz.de.boulange@samorzadsgh.pl>
2019-11-29 16:51:18 +0100

    Kod z zajec nr 2

--- src/main/java/org/skni/student_was/domain/University.java
+++ src/main/java/org/skni/student_was/domain/University.java
@@ -1,14 +1,53 @@
 package org.skni.student_was.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component("uni")
 public class University {
 
-    public String name= "SGH";
+    @Value("${university.name:WAT}")
+    private String name;
 
-    public University (){
+    private Student student;
 
+    @Autowired
+    public University (Student student) {
+        this.student = student;
     }
 
+    University (Student student, String name) {
+        this.student = student;
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void build () {
+        System.out.println("Bean University " + name + " has bean created");
+    }
+
+    @PreDestroy
+    public void tearDown () {
+        System.out.println("Bean University " + name + " is being destroyed");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    @Override
+    public String toString() {
+        return "University[" +
+                "name=" + this.name +
+                ",studentName=" + this.student.getName() +
+                "]";
+    }
 }
---
+++ src/test/java/org/skni/student_was/domain/UniversityTest.java
@@ -0,0 +1,17 @@
+package org.skni.student_was.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UniversityTest {
+
+    @Test
+    public void universityTest () {
+        Task task = new Task();
+        Student student = new Student(task, "Tomek", 3);
+        University university = new University(student, "PW");
+        String expectedValue = "University[name=PW,studentName=Tomek]";
+        assertEquals(expectedValue, university.toString());
+    }
+}
