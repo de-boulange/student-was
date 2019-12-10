ecc3e8b316f515479cb6b49c9cb56604898be2ae
de-boulange <bartosz.de.boulange@samorzadsgh.pl>
2019-12-10 16:43:03 +0100

    poprawki

--- src/main/java/org/skni/student_was/StudentWasApplication.java
+++ src/main/java/org/skni/student_was/StudentWasApplication.java
@@ -2,8 +2,10 @@ package org.skni.student_was;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 //@ComponentScan(basePackages = "org.skni")
 //@ImportResource("config/spring-beans.xml")
 public class StudentWasApplication {
