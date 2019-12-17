e2a9ad6fa6fb59b0e9bfafdc5269de21512a6531
de-boulange <bartosz.de.boulange@samorzadsgh.pl>
2019-12-17 15:36:47 +0100

    dodanie sciagawki html i sql na zajecia 5

---
+++ sciagawka/sql-template.sql
@@ -0,0 +1,4 @@
+INSERT INTO Task (id,opis) VALUES (0,'Zadanie z mikroekonomii');
+INSERT INTO Task (id,opis) VALUES (1,'Napisanie eseju nt. Javy');
+INSERT INTO Task (id,opis) VALUES (2,'Napisanie programu w Java Spring');
+INSERT INTO Task (id,opis) VALUES (3,'Zadanie z algebry liniowej');
